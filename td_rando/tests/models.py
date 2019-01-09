297dfdd479fb9187d86752330894731e5fcc3706
Titus Diphale <tdiphale@bhp.org.bw>
2019-01-10 01:05:14 +0200

    dummy model

--- td_rando/tests/models.py
+++ td_rando/tests/models.py
@@ -1,5 +1,8 @@
 from django.db import import models
+from django.db.models.deletion import PROTECT
 from edc_base.model_mixins import BaseUuidModel
+from edc_base.utils import get_utcnow
+from edc_registration.model_mixins import UpdatesOrCreatesRegistrationModelMixin
 
 
 class MaternalRando (BaseUuidModel):
@@ -19,4 +22,39 @@ class MaternalRando (BaseUuidModel):
     randomization_datetime=models.DateTimeField()
     
     initials=models.Charfield(max_length=25)
+
+class Appointment(BaseUuidModel):
+
+    subject_identifier = models.CharField(max_length=25)
+
+    appt_datetime = models.DateTimeField(default=get_utcnow)
+
+    visit_code = models.CharField(max_length=25)
+
+    visit_instance = models.CharField(max_length=25)
+    
+class MaternalVisit(BaseUuidModel):
+
+    subject_identifier = models.CharField(max_length=25)
+
+    appointment = models.OneToOneField(Appointment, on_delete=PROTECT)
+    
+class RegisteredSubject(BaseUuidModel):
+
+    subject_identifier = models.CharField(
+        max_length=50,
+        unique=True)
+
+    relative_identifier = models.CharField(
+        max_length=36,
+        null=True,
+        blank=True)
+
+class MaternalConsent(UpdatesOrCreatesRegistrationModelMixin, BaseUuidModel):
+
+    subject_identifier = models.CharField(max_length=25)
+
+    consent_datetime = models.DateTimeField()
+
+    dob = models.DateField()            
     
