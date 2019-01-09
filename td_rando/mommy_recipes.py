025578f5a623cd8874e75b9559bcf1374f508e47
Titus Diphale <tdiphale@bhp.org.bw>
2019-01-09 10:58:23 +0200

    mommy recipe

---
+++ td_rando/mommy_recipes.py
@@ -0,0 +1,14 @@
+from edc_base.utils import get_utcnow
+from model_mommy.recipe import Recipe
+from td_maternal.models import MaternalRando
+
+
+maternalrando = Recipe(
+    MaternalRando,
+    report_datetime=get_utcnow(),
+    site='Gaborone',
+    sid=1,
+    rx='NVP',
+    subject_identifier='085-0000000-1',
+    randomization_datetime=get_utcnow(),
+    initials='IN')
